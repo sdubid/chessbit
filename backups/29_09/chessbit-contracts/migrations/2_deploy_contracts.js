cd3f0ee31b8e5097187cc230ff542d4897509bb0
Sdubid <leons.lenkovs@gmail.com>
2024-10-17 20:58:26 +0200

    Initial project setup with frontend and backend

--- backups/29_09/chessbit-contracts/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-const ChessGame = artifacts.require("ChessGame");
-
-module.exports = function (deployer) {
-  deployer.deploy(ChessGame);
-};
--- backups/chessbit-contracts/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-// migrations/2_deploy_contracts.js
-
-const ChessGame = artifacts.require("ChessGame");
-
-module.exports = function (deployer) {
-  deployer.deploy(ChessGame);
-};
